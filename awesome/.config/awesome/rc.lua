8a48037337603d4664375023e0ac3ba12838b98f
paulogitbox <152565276+PauloGitbox@users.noreply.github.com>
2024-02-26 02:11:04 +0000

    --experimental-backgrounds

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -775,7 +775,7 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 -- Autostart Applications
 awful.spawn.with_shell("sleep 0.5 && ~/.fehbg")
 awful.spawn.with_shell("setxkbmap -layout 'us,gb' -option 'grp:win_space_toggle'")
+-- awful.spawn.with_shell("sleep 2 && picom --experimental-backends --daemon")
 -- awful.spawn.with_shell("sleep 1 && gpick")
 -- awful.spawn.with_shell("sleep 1 && flameshot")
 -- awful.spawn.with_shell("sleep 2 && nm-applet")
--- awful.spawn.with_shell("sleep 5 && picom --daemon")
